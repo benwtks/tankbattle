58fae38fedbeb6b44f1a74d85d5c590a882c1f2b
Ben Watkins <contact@benwtks.com>
2021-02-13 22:59:37 +0000

    Add sockets.io

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-const express = require('express')
+const express = require('express');
+const socketio = require('socket.io');
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-	  res.send('Hello World!')
+const server = app.listen(port, () => {
+	  console.log(`Example app listening at http://localhost:${port}`)
 })
 
-app.listen(port, () => {
-	  console.log(`Example app listening at http://localhost:${port}`)
+// Setup socket.io
+const io = socketio(server);
+
+// Listen for socket.io connections
+io.on('connection', socket => {
+  console.log('Player connected!', socket.id);
+
+  socket.on(Constants.MSG_TYPES.JOIN_GAME, joinGame);
+  socket.on(Constants.MSG_TYPES.INPUT, handleInput);
+  socket.on('disconnect', onDisconnect);
+});
+
+app.get('/', (req, res) => {
+	  res.send('Hello World!')
 })
