10100e2971e4f324cf2dabc2bafe23b25c7ca052
Ben Watkins <contact@benwtks.com>
2021-02-14 02:08:06 +0000

    Update api

--- README.md
+++ README.md
@@ -1,10 +1,30 @@
 # tankbattle
 Multiplayer tank trouble game in Node.js
 
-# Instructions
+# Development
 
 To install dependencies
 `npm install`
 
 To start the server
 `npm start`
+
+# WebSocket API Messages
+
+Messages between the client and server.
+
+## Messages to server
+
+```
+start_game
+join_game <id>
+shoot <timestamp>
+client_update <x> <y> <theta>
+```
+
+## Messages to client
+
+```
+game_over
+server_update [(<bullet x>,<bullet y>,<vx>,<vy>)] [(<player id>,<x>,<y>, <theta>)]
+```
--- constants.js
+++ constants.js
@@ -1,21 +1,23 @@
 module.exports = Object.freeze({
-  PLAYER_RADIUS: 20,
-  PLAYER_MAX_HP: 100,
-  PLAYER_SPEED: 400,
-  PLAYER_FIRE_COOLDOWN: 0.25,
+	PLAYER_RADIUS: 20,
+	PLAYER_MAX_HP: 100,
+	PLAYER_SPEED: 400,
+	PLAYER_FIRE_COOLDOWN: 0.25,
 
-  BULLET_RADIUS: 3,
-  BULLET_SPEED: 800,
-  BULLET_DAMAGE: 10,
+	BULLET_RADIUS: 3,
+	BULLET_SPEED: 800,
+	BULLET_DAMAGE: 10,
 
-  SCORE_BULLET_HIT: 20,
-  SCORE_PER_SECOND: 1,
+	SCORE_BULLET_HIT: 20,
+	SCORE_PER_SECOND: 1,
 
-  MAP_SIZE: 3000,
-  MSG_TYPES: {
-    JOIN_GAME: 'join_game',
-    GAME_UPDATE: 'update',
-    INPUT: 'input',
-    GAME_OVER: 'dead',
-  },
+	MAP_SIZE: 3000,
+	MSG_TYPES: {
+		START_GAME: 'start_game',       // Client starts a game
+		JOIN_GAME: 'join_game',         // Client requests to join a game
+		SHOOT: 'shoot',                 // client telling server it has shoot
+		CLIENT_UPDATE: 'client_update', // client updating the server about its user
+		SERVER_UPDATE: 'server_update'  // server updating everyone
+		GAME_OVER: 'game_over',         // Server tells clients the game has finished
+	},
 });
