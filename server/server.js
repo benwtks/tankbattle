7512f86879bc59b3750f99a2669400b7a7dc5958
Ben Watkins <contact@benwtks.com>
2021-02-14 01:13:14 +0000

    Fix sendFile

--- server/server.js
+++ server/server.js
@@ -1,16 +1,19 @@
-const api = require('./api');
-
+const path = require('path')
 const express = require('express');
 
 const app = express()
 const port = 3000
 
+const api = require('./api');
+
 const server = app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`)
+	console.log(path.resolve(__dirname, '../client/index.html'))
 })
 
 app.get('/', (req, res) => {
-	res.sendFile('client/index.html', {root: __dirname })
+	res.sendFile(path.resolve(__dirname, '../client/index.html'))
 })
 
-api.listenForSockets(server);
+
+api.listenForSockets();
