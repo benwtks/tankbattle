930f88b235359904de7ab32ecb1d13d9d707e960
Giorgio Momigliano <gmomigliano@protonmail.com>
2021-02-14 19:12:23 +0000

    Changing server updates

--- server/websockets/send.js
+++ server/websockets/send.js
@@ -2,19 +2,22 @@ const Constants = require('../../constants');
 
 
 function sendUpdate(player, leaderboard) {
-	const nearbyPlayers = Object.values(this.players).filter(
-		p => p !== player && p.distanceTo(player) <= Constants.MAP_SIZE / 2,
-	);
-	const nearbyBullets = this.bullets.filter(
-		b => b.distanceTo(player) <= Constants.MAP_SIZE / 2,
-	);
+	// const nearbyPlayers = Object.values(this.players).filter(
+	// 	p => p !== player && p.distanceTo(player) <= Constants.MAP_SIZE / 2,
+	// );
+	// const nearbyBullets = this.bullets.filter(
+	// 	b => b.distanceTo(player) <= Constants.MAP_SIZE / 2,
+	// );
 
+	// socket.emit(Constants.MSG_TYPES.SERVER_UPDATE, {
+	// 	t: Date.now(),
+	// 	me: player.serializeForUpdate(),
+	// 	others: nearbyPlayers.map(p => p.serializeForUpdate()),
+	// 	bullets: nearbyBullets.map(b => b.serializeForUpdate()),
+	// 	leaderboard,
+	// });
 	socket.emit(Constants.MSG_TYPES.SERVER_UPDATE, {
-		t: Date.now(),
-		me: player.serializeForUpdate(),
-		others: nearbyPlayers.map(p => p.serializeForUpdate()),
-		bullets: nearbyBullets.map(b => b.serializeForUpdate()),
-		leaderboard,
+		msg: "hi",
 	});
 }
 
